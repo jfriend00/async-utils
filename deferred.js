c555d56758e840cb4e9e99c295baefa66004cf99
jfriend00 <jfriend00@users.noreply.github.com>
2020-04-25 14:57:53 -0700

    Add promiseAllDone

--- deferred.js
+++ deferred.js
@@ -19,6 +19,23 @@ function Deferred() {
     }
 }
 
+// like Promise.allSettled in that it doesn't resolve until all promises are done
+// But it will still reject with the first error if there were any errors
+// If there were no errors, it resolves with the full array of results
+// Use it when you don't want it to resolve until all operations are done, but stil
+// want a quick way to know when there's an error
+function promiseAllDone(promises) {
+    return Promise.allSettled(promises).then(results => {
+        return results.map(r => {
+            if (r.status === "rejected") {
+                // reject with first error we found
+                throw r.reason;
+            }
+            return r.value;
+        });
+    });
+}
+
 // Like Promise.allSettled, but instead of resolving to an array of objects that have
 // the status in it, the array is just the resolved values with a sentinel errorVal
 // in place of the errors (often null)
@@ -57,4 +74,4 @@ function promiseTimeout(p, t, e) {
     });
 }
 
-module.exports = { Deferred, promiseSettleWithVal, promiseTimeout };
+module.exports = { Deferred, promiseSettleWithVal, promiseTimeout, promiseAllDone };
