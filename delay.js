7a0caef7004e474103bec905e099a101fa0ada18
jfriend00 <jfriend00@users.noreply.github.com>
2020-04-25 16:07:57 -0700

    Delete delay.js
    Moved code to utils.js

--- delay.js
+++
@@ -1,8 +0,0 @@
-// return a promise that resolves after a delay
-function delay(t, val) {
-   return new Promise(function(resolve) {
-       setTimeout(resolve, t, val);
-   });
-}
-
-module.exports = { delay };
