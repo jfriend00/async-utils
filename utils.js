98181a3680012402077ca24f5c4eccff60b2dcc4
jfriend00 <jfriend00@users.noreply.github.com>
2020-05-06 19:51:42 -0700

    Add peekIterator functions

--- utils.js
+++ utils.js
@@ -1,12 +1,65 @@
-// like Promise.allSettled in that it doesn't resolve until all promises are done
-// But it will still reject with the first error if there were any errors
-// If there were no errors, it resolves with the full array of results
-// Use it when you don't want it to resolve until all operations are done, but stil
-// want a quick way to know when there's an error
+
+// Wraps an existing iterator and proxies it's .next().  But, adds a .peek() method that lets you look ahead
+// and see if there are any more values before you act on them.  It allows you to make logic decisions
+// based on whether there are any more values before you've permanently fetched the value.
+// peek() actually causes the value to be retrieved from the original iterator when it is called so if there
+// are side effects from retrieving a value from the original iterator, those side effecdts will occur when peek()
+// is called, not when next() is called.
+// You can call peek() as many times as you want on the same value.  Once it has retrieved the value from
+// the original iterator, it caches that value until you call next() to fetch it.  You MUST call next() to
+// be able to advance the iterator.  Repeated calls to peek() will just return the same value over and over.
+function getPeekIterator(iterator) {
+    // three states: "noValueCached", "valueCached", "done"
+    let state = "noValueCached";
+    let value;
+    let newIterator = {};
+    // retrieve next value without advanced iterator
+    newIterator.peek = function() {
+        if (state === "done") return {done: true};
+        if (state === "valueCached") return value;
+        // state must be "noValueCached"
+        value = iterator.next();
+        state = value.done ? "done" : "valueCached";
+        return value;
+    }
+    // retrieve next value, advance iterator
+    newIterator.next = function() {
+        let nextVal = newIterator.peek();
+        if (!nextVal.done) {
+            // by putting the state to noValueCached, we "take" the value out of the cache
+            // so the next time peek() is called it will get a new value
+            state = "noValueCached";
+        }
+        return nextVal;
+    }
+    // simple boolean wrapper around .peek()
+    // returns true if there's more data, returns false is there's no more data
+    newIterator.isMore = function() {
+        let val = newIterator.peek();
+        return !val.done;
+    }
+    return newIterator;
+}
+
+// Pass an iterable (like an Array or Set or Map) and it will get the default iterator
+// and pass that to getPeekIterator() for you to get a peekIterator for that default iterator
+function getDefaultPeekIterator(iterable) {
+    return getPeekIterator(iterable[Symbol.iterator]());
+}
+
+// Use promiseAllDone() when you want two possible outcomes:
+// 1) If all promises resolve, you get an array of simple results (same output as Promise.all())
+// 2) If any promise rejects, you get the first rejection as a rejected promise, but you don't
+//    get the rejection until all the promises are completed in some way.
+// So, it differs from Promise.all() in that it does not finish until all the promises have
+//    finished, even if there's a rejection
+// It differs from Promise.allSettled() in two ways.  First, it will reject if any promises
+//    reject whereas Promise.allSettled() never rejects.  Second, when it resolves, it
+//    resolves to a simple array of results, not an arrray of objects.
 function promiseAllDone(promises) {
     return Promise.allSettled(promises).then(results => {
         return results.map(r => {
-            if (r.status === "rejected") {
+            if (r.status === 'rejected') {
                 // reject with first error we found
                 throw r.reason;
             }
@@ -22,7 +75,7 @@ function promiseAllDone(promises) {
 function promiseSettleWithVal(promises, errorVal = null) {
     return Promise.allSettled(promises).then(results => {
         return results.map(result => {
-            return result.status === 'filfilled' ? result.value : errorVal;
+            return result.status === 'fulfilled' ? result.value : errorVal;
         })
     });
 }
@@ -91,4 +144,13 @@ function promisifyAll(obj, suffix = "Async") {
 }
 
 
-module.exports = { promiseSettleWithVal, promiseTimeout, promiseAllDone, delay, promisifyAll, promisifyObj };
+module.exports = {
+    promiseSettleWithVal,
+    promiseTimeout,
+    promiseAllDone,
+    delay,
+    promisifyAll,
+    promisifyObj,
+    getPeekIterator,
+    getDefaultPeekIterator,
+ };
